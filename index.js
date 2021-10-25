343f3e2a161ee9b14b26233f7526fc2ce115b6b8
cbdyzj <cbdyzj@jianzhao.org>
2021-10-25 22:41:53 +0800

    Init

---
+++ index.js
@@ -0,0 +1,23 @@
+const { join } = require('path')
+const { createProxyServer } = require('http-proxy')
+const express = require('express')
+
+const app = express()
+
+app.use(express.static(join(__dirname, './static')))
+
+app.get('/api/*', (() => {
+    const proxy = createProxyServer({ target: 'http://localhost:8080/' })
+
+    proxy.on('error', (err, req, res) => {
+        res.writeHead(500, { 'Content-Type': 'text/plain' })
+        res.end(err.toString());
+    })
+
+    return (req, res) => proxy.web(req, res)
+})())
+
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log('Serving on ' + PORT)
+})
